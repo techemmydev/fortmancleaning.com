1ff14d1852f4142a38f84e2bb2eacac21d243ad5
techemmydev <139622014+techemmydev@users.noreply.github.com>
2023-08-11 03:37:06 +0100

    Add files via upload

---
+++ javaprogram/index.js
@@ -0,0 +1,19 @@
+
+/*
+let preloader = document.querySelector(".preloader");
+window.addEventListener("load" ,fadeout);
+
+function faderout(){
+    preloader.classlist.add("hide");
+}
+*/
+
+var loader = document.querySelector(".loader");
+window.addEventListener("load", vanish);
+
+function vanish() {
+    loader.classList.add("disapper")
+}
+
+https://docs.google.com/forms/d/1yF8TrYj3xr8gRyRAGCG9zyNoVrzGmQI8nCOetnPmzFY/edit" target="_blank" title="complete your order">
+
